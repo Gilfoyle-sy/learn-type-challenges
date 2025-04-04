041f6711ad988f3b51114e456f8fc622e11454b4
Song <keyoos@163.com>
2025-04-04 15:03:34 +0800

    fix: 00003 omit

--- src/medium/00003-omit/template.ts
+++ src/medium/00003-omit/template.ts
@@ -1,6 +1,8 @@
-type MyOmit<T, K> = {
+type MyOmit<T, K extends keyof T> = {
   [P in keyof T as P extends K ? never : P]: T[P]
 }
 
-// in keyof 是遍历T的key
-// as P extends K ? never : P 是判断P是否是K的子集，如果是，则不返回P
+// extends keyof 表示 K 必须是 T 的 key
+// in keyof T 表示遍历 T 的 key
+// as P extends K ? never : P 表示如果 P 是 K 的子集，则忽略，否则保留
+// T[P] 表示保留 T 的值
